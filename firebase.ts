e90463bdf87d074b8e94b21210834a9b1237151c
haddoke2960 <haddoke@gmail.com>
2025-04-28 22:17:08 -0400

    git commit -m "feat: connect Firebase and implement Firestore save + clear functionality"

--- firebase.ts
+++ firebase.ts
@@ -1,5 +1,5 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -11,6 +11,4 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-export { db };
+export const db = getFirestore(app);
