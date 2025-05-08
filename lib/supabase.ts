e3021b25cbf31e7b0caf798c46b28d644b1042b6
haddoke2960 <haddoke@gmail.com>
2025-05-07 23:37:27 -0400

    Update supabase.ts
    configure Supabase client with project URL and anon key

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://YOUR_PROJECT_ID.supabase.co';
-const supabaseKey = 'YOUR_ANON_KEY';
+const supabaseUrl = 'https://jwdiqetmiheowrezorbg.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp3ZGlxZXRtaWhlb3dyZXpvcmJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY2NjY1OTQsImV4cCI6MjA2MjI0MjU5NH0.-m8rFYr_pps_xB_ZOl9U2l-qgfauT8NHmkXcWCdBRtI';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
