f86d4da944fcea5499de5f11b128be7c34a374f2
haddoke2960 <haddoke@gmail.com>
2025-05-09 22:45:06 -0400

    Update _document.tsx
    chore: update _document.tsx to include manifest and PWA meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/icons/icon-192x192.png" />
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+        <meta name="theme-color" content="#0070f3" />
+      </Head>
       <body>
         <Main />
         <NextScript />
