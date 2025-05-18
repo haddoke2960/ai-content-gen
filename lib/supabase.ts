fc622cd3e2f808117caa7326a043e86e13ee5408
haddoke2960 <haddoke@gmail.com>
2025-05-18 10:27:54 -0400

    Update supabase.ts
    Switch supabase config to use Vercel env vars

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://jwdiqetmiheowrezorbg.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp3ZGlxZXRtaWhlb3dyZXpvcmJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY2NjY1OTQsImV4cCI6MjA2MjI0MjU5NH0.-m8rFYr_pps_xB_ZOl9U2l-qgfauT8NHmkXcWCdBRtI';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
