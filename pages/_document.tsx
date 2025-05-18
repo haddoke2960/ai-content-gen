6d95ed5b94dbdf8eefb4a61907d1dba8abe753f9
haddoke2960 <haddoke@gmail.com>
2025-05-17 20:54:55 -0400

    Update _document.tsx
    style: replace document to include Inter font and modern meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,12 @@ export default function Document() {
         <link rel="icon" type="image/png" sizes="192x192" href="/icons/icon-192x192.png" />
         <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
         <meta name="theme-color" content="#0070f3" />
+
+        {/* Google Fonts: Inter */}
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <body>
         <Main />
